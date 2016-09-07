a7505ed3897b2ad6842e17f783d92742c6d3c1cf
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-09-07 11:39:13 +0900

    initial implementation

--- test/serverspec/default_spec.rb
+++ test/serverspec/default_spec.rb
@@ -1,58 +1,21 @@
 require 'spec_helper'
 require 'serverspec'
 
-package = 'ospfd'
 service = 'ospfd'
-config  = '/etc/ospfd/ospfd.conf'
-user    = 'ospfd'
-group   = 'ospfd'
-ports   = [ PORTS ]
-log_dir = '/var/log/ospfd'
-db_dir  = '/var/lib/ospfd'
-
-case os[:family]
-when 'freebsd'
-  config = '/usr/local/etc/ospfd.conf'
-  db_dir = '/var/db/ospfd'
-end
-
-describe package(package) do
-  it { should be_installed }
-end 
+config  = '/etc/ospfd.conf'
+user    = '_ospfd'
+group   = '_ospfd'
 
 describe file(config) do
   it { should be_file }
-  its(:content) { should match Regexp.escape('ospfd') }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when 'freebsd'
-  describe file('/etc/rc.conf.d/ospfd') do
-    it { should be_file }
-  end
+  #its(:content) { should match Regexp.escape('ospfd') }
+  its(:content) { should match /^auth-type crypt$/ }
+  its(:content) { should match /^auth-md-keyid 1$/ }
+  its(:content) { should match /^redistribute static$/ }
+  its(:content) { should match /^area 0.0.0.0 {\n\s+interface em0 { passive }\n}/ }
 end
 
 describe service(service) do
   it { should be_running }
   it { should be_enabled }
-end
-
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
-  end
 end
